9fdc5e64589dc11117f634b87324d94df6935024
id2k1149 <id2k1149@gmail.com>
2023-05-10 10:25:21 -0400

    Initial Commit

---
+++ PulsatingButton/PulsatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsatingButtonApp.swift
+//  PulsatingButton
+//
+//  Created by Max Franz Immelmann on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PulsatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
